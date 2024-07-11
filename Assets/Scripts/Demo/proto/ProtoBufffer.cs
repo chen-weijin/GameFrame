c64d9c347670515264e7f4e5a221b48a48f6a355
chenweijin <chenweijin@weile.com>
2024-07-11 19:04:23 +0800

    增加protodemo

---
+++ Assets/Scripts/Demo/proto/ProtoBufffer.cs
@@ -0,0 +1,21 @@
+﻿using Google.Protobuf;
+public class ProtoBufffer
+{
+    public static byte[] Serialize(IMessage message)
+    {
+        return message.ToByteArray();
+    }
+
+    public static T DeSerialize<T>(byte[] packet) where T : IMessage, new()
+    {
+        IMessage message = new T();
+        try
+        {
+            return (T)message.Descriptor.Parser.ParseFrom(packet);
+        }
+        catch (System.Exception e)
+        {
+            throw;
+        }
+    }
+}
