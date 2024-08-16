7c9c3a8fdb24324847b0e16723ddfd6f6accc04d
chenweijin <chenweijin@weile.com>
2024-08-16 15:09:01 +0800

    增加瓦片地图

---
+++ Assets/Scripts/View/BattleView.cs
@@ -0,0 +1,29 @@
+﻿using framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using WeChatWASM;
+
+public class BattleView : BaseView
+{
+    protected override void Start()
+    {
+        var g = GetGrid();
+        var person = g.gameObject.FindInChildren("Person");
+    }
+
+    public Transform GetGrid()
+    {
+        var s = SceneManager.GetActiveScene();
+        var gArr = s.GetRootGameObjects();
+        for (var i = 0; i < gArr.Length; i++)
+        {
+            if (gArr[i].name == "Grid")
+            {
+                return gArr[i].transform;
+            }
+        }
+        return null;
+    }
+
+}
